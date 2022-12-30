a8ebfb250c546feaa650a17e4502274d07f2abbd
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-12-29 20:00:19 -0600

    volume rocker now functional

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,34 @@
-const pianoKeys = document.querySelectorAll(".piano-keys .key")
+const pianoKeys = document.querySelectorAll(".piano-keys .key"),
+volumeSlider = document.querySelector(".volume-slider input");
 
 
-let audio = new Audio("tunes/a.wav");
+let allKeys = [],
+audio = new Audio("tunes/a.wav");
+
 const playTune = (key) => {
- audio.play();
-} 
+    audio.src = `tunes/${key}.wav`;
+    audio.play(); 
+    
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(()=> {
+        clickedKey.classList.remove("active");
+    }, 150)
+}
 
 
 pianoKeys.forEach(key => {
-    key.addEventListener("click", ()=> playTune(key.dataset.key));
+    allKeys.push(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
 });
+
+const handleVolume = (e) => {
+audio.volume = e.target.value;
+}
+
+const pressedKey = (e) => {
+   if(allKeys.includes(e.key)) playTune(e.key);
+}
+volumeSlider.addEventListener("input", handleVolume)
+document.addEventListener("keydown", pressedKey)
